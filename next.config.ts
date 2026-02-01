bffcb25bc98b53355f8b9a046caa7ee10fe03e56
ourrapartment10-dot <ourrapartment10@gmail.com>
2026-02-01 12:28:15 +0530

    chore: Remove webpack configuration for suppressing DEP0169 warnings.

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Suppress DEP0169 warning from url.parse() in dependencies
-      const originalEmitWarning = process.emitWarning;
-      process.emitWarning = (warning: any, ...args: any[]) => {
-        if (
-          typeof warning === 'string' &&
-          warning.includes('DEP0169')
-        ) {
-          return;
-        }
-        return originalEmitWarning.call(process, warning, ...args);
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
+
